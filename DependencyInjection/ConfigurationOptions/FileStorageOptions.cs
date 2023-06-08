6a00032414e57eec6db21a1da3734981ac9c02a9
Sajad Delavari <39267124+sajaddelavari@users.noreply.github.com>
2023-06-08 13:48:06 +0330

    Initial commit

---
+++ DependencyInjection/ConfigurationOptions/FileStorageOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace Havit.NewProjectTemplate.DependencyInjection.ConfigurationOptions;
+
+public class FileStorageOptions
+{
+	public const string FileStorageOptionsKey = "AppSettings:FileStorage";
+
+	public string PathOrContainerName { get; set; }
+}
---
+++ Services/HealthChecks/NewProjectTemplateDbContextHealthCheck.cs
@@ -0,0 +1,21 @@
+﻿using Havit.Data.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Havit.NewProjectTemplate.Services.HealthChecks;
+
+public class NewProjectTemplateDbContextHealthCheck : BaseHealthCheck
+{
+	private readonly IDbContext dbContext;
+
+	public NewProjectTemplateDbContextHealthCheck(IDbContext dbContext)
+	{
+		this.dbContext = dbContext;
+	}
+
+	protected async override Task<HealthCheckResult> CheckHealthAsync(CancellationToken cancellationToken)
+	{
+		return await dbContext.Database.CanConnectAsync(cancellationToken)
+			? HealthCheckResult.Healthy()
+			: HealthCheckResult.Unhealthy();
+	}
+}
